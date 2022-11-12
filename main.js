cd048e0273a8298388d46a3b27315e0068fc0de8
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-12 07:28:04 +0530

    promises theory done

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function importantAction1(message) {
 function importantAction2(msg) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      reject(`Step 2 - ${msg}`);
+      resolve(`Step 2 - ${msg}`);
     }, 600);
   });
 }
@@ -18,104 +18,38 @@ function importantAction3(msg) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(`Step 3 - ${msg}`);
-    }, 10);
+    }, 1000);
   });
 }
 
-// Below is Promise combinators - promise.any - works exactly
-// like promise.race, but it returns the first fulfilled promise and ignores the rest.
+// Async Await - Best approach for executing promises one after the other.
 
-Promise.any([
-  importantAction1("I would say that,"),
-  importantAction2("this example is awesome!"),
-  importantAction3("Trying to appreciate promises!"),
-  importantAction3("Now I understand promise chaining, phew!!"),
-])
-  .then((result) => console.log(result))
-  .catch((error) => console.error("Error: Promise failed", error));
+//With try catch block
 
-// Below is Promise combinators - promise.allSettled - works exactly
-// like promise.all, but returns failed ones as well as the fullfilled promises.
+const result = async () => {
+  try {
+    const message1 = await importantAction1("I, ");
+    console.log(message1);
+    const message2 = await importantAction2("Understand, ");
+    console.log(message2);
+    const message3 = await importantAction3("Promises.");
+    console.log(message3);
 
-// Promise.allSettled([
-//   importantAction1("I would say that,"),
-//   importantAction2("this example is awesome!"),
-//   importantAction3("Trying to appreciate promises!"),
-//   importantAction3("Now I understand promise chaining, phew!!"),
-// ])
-//   .then((result) => console.log(result))
-//   .catch((error) => console.error("Error: Promise failed", error));
+    console.log(message1, message2, message3);
+  } catch (error) {
+    console.error("Promise failed", error);
+  }
+};
 
-// Below is Promise combinators - promise.race - returns the one which
-// fails or succeeds first.
+result();
 
-// Promise.race([
-//   importantAction1("I would say that,"),
-//   importantAction2("this example is awesome!"),
-//   importantAction3("Trying to appreciate promises!"),
-//   importantAction3("Now I understand promise chaining, phew!!"),
-// ])
-//   .then((result) => console.log(result))
-//   .catch((error) => console.error("Error: Promise failed", error));
+//Without try catch block
+// const result = async () => {
+//   const message1 = await importantAction1("I, ");
+//   const message2 = await importantAction2("Understand, ");
+//   const message3 = await importantAction3("Promises.");
 
-// Below is Promise combinators - promise.all - even if one of the promise fails,
-// the whole thing fails.
+//   console.log(message1, message2, message3);
+// };
 
-// Promise.all([
-//   importantAction1("I would say that,"),
-//   importantAction2("this example is awesome!"),
-//   importantAction3("Trying to appreciate promises!"),
-//   importantAction3("Now I understand promise chaining, phew!!"),
-// ])
-//   .then((result) => console.log(result))
-//   .catch((error) => console.error("Error: Promise failed", error));
-
-// Below is the promise chaining
-
-// importantAction1("I would say that,")
-//   .then((res) => {
-//     console.log(res);
-//     return importantAction2("this example is awesome!");
-//   })
-//   .then((res) => {
-//     console.log(res);
-//     return importantAction3("Trying to appreciate promises!");
-//   })
-//   .then((res) => {
-//     console.log(res);
-//     return importantAction3("Now I understand promise chaining, pheww!!");
-//   })
-//   .then((res) => {
-//     console.log(res);
-//   })
-//   .catch((err) => console.error(err));
-
-// Below is the promise approach
-
-// importantAction1("I would say that,")
-//   .then((res) => {
-//     console.log(res);
-//     importantAction2("this example is awesome!").then((res) => {
-//       console.log(res);
-//     });
-//     importantAction3("Trying to appreciate promises!").then((res) => {
-//       console.log(res);
-//     });
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-//
-
-// Below is the call back hell
-
-// const msg = importantAction1("I would say that,", function (message) {
-//   console.log(message);
-//   importantAction2("this example is awesome!", (msg) => console.log(msg));
-//   importantAction3(
-//     "I think I am understanding call back hell and pyramid of doom!",
-//     (msg) => console.log(msg)
-//   );
-// });
-
-// Well, comes to rescue promises!
+// result();
