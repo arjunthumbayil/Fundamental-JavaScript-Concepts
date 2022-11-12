61b15cc205dafffc979ac9b5d71134ff95263d51
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-12 06:27:22 +0530

    promise.all

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function importantAction1(message) {
 function importantAction2(msg) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(`Step 2 - ${msg}`);
+      reject(`Step 2 - ${msg}`);
     }, 600);
   });
 }
@@ -22,27 +22,40 @@ function importantAction3(msg) {
   });
 }
 
+// Below is Promise combinators - promise.all - even if one of the promise fails,
+// the whole thing fails.
+
+Promise.all([
+  importantAction1("I would say that,"),
+  importantAction2("this example is awesome!"),
+  importantAction3("Trying to appreciate promises!"),
+  importantAction3("Now I understand promise chaining, phew!!"),
+])
+  .then((result) => console.log(result))
+  .catch((error) => console.error("Error: Promise failed", error));
+
 // Below is the promise chaining
 
-importantAction1("I would say that,")
-  .then((res) => {
-    console.log(res);
-    return importantAction2("this example is awesome!");
-  })
-  .then((res) => {
-    console.log(res);
-    return importantAction3("Trying to appreciate promises!");
-  })
-  .then((res) => {
-    console.log(res);
-    return importantAction3("Now I understand promise chaining, pheww!!");
-  })
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => console.error(err));
+// importantAction1("I would say that,")
+//   .then((res) => {
+//     console.log(res);
+//     return importantAction2("this example is awesome!");
+//   })
+//   .then((res) => {
+//     console.log(res);
+//     return importantAction3("Trying to appreciate promises!");
+//   })
+//   .then((res) => {
+//     console.log(res);
+//     return importantAction3("Now I understand promise chaining, pheww!!");
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => console.error(err));
 
 // Below is the promise approach
+
 // importantAction1("I would say that,")
 //   .then((res) => {
 //     console.log(res);
@@ -57,7 +70,9 @@ importantAction1("I would say that,")
 //     console.log(err);
 //   });
 //
+
 // Below is the call back hell
+
 // const msg = importantAction1("I would say that,", function (message) {
 //   console.log(message);
 //   importantAction2("this example is awesome!", (msg) => console.log(msg));
