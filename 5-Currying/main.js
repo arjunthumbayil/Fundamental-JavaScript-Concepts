216a5bdbe1c6eaf14fb1a536d84b9330a7e5a54d
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-19 06:45:25 +0530

    operation("?")(x)(y)

--- 5-Currying/main.js
+++ 5-Currying/main.js
@@ -5,17 +5,46 @@ And curried functions are constructed by chaining closures by immediately return
 inner functions simultaneously.
 */
 
-//Question 1 - sum(2)(6)(1)?
+/*Question 2 -
+   evaluate("add")(4)(2) => 6
+   evaluate("multiply")(4)(2) => 8
+   evaluate("divide")(4)(2) => 2
+   evaluate("substract")(4)(2) => 2
+   */
 
-function sum(a) {
-  return function (b) {
-    return function (c) {
-      return a + b + c;
+function evaluate(operation) {
+  return function (x) {
+    return function (y) {
+      if (operation === "add") return x + y;
+      else if (operation === "multiply") return x * y;
+      else if (operation === "divide") return x / y;
+      else if (operation === "substract") return x - y;
+      else return "Invalid! Please provide a valid operation(+,-,/,*).";
     };
   };
 }
 
-console.log(sum(2)(6)(1));
+console.log(evaluate("add")(4)(2));
+console.log(evaluate("multiply")(4)(2));
+console.log(evaluate("divide")(4)(2));
+console.log(evaluate("substract")(4)(2));
+console.log(evaluate("integrate")(4)(2));
+
+/* Correct Output
+
+ */
+
+//Question 1 - sum(2)(6)(1)?
+
+// function sum(a) {
+//   return function (b) {
+//     return function (c) {
+//       return a + b + c;
+//     };
+//   };
+// }
+
+// console.log(sum(2)(6)(1));
 
 /* Correct Output
 
