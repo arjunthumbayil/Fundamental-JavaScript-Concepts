fcae8df33db939838b00bb20a447117ac630ddb1
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-22 06:13:04 +0530

    JSON.stringify and JSON.parse

--- 6-Objects/main.js
+++ 6-Objects/main.js
@@ -4,18 +4,45 @@ a name(or key) and a value. A property's value can be a function, in which case 
 property is known as a method.
 */
 
+/* Question 4 - What's JSON.stringify and JSON.parse ? */
+
+const obj = { name: "abc", marks: 99, address: "bcd_street" };
+const strObj = JSON.stringify(obj);
+console.log(strObj); // {"name":"abc","marks":99,"address":"bcd_street"}
+
+console.log(strObj.name); // is undefined as strObj is a string now
+
+const objObj = JSON.parse(strObj);
+console.log(objObj); // {"name":"abc","marks":99,"address":"bcd_street"}
+
+localStorage.setItem("test", strObj);
+console.log(JSON.parse(localStorage.getItem("test"))); /* {
+    "name": "abc",
+    "marks": 99,
+    "address": "bcd_street"
+}
+*/
+
+console.log(JSON.stringify(obj, ["marks"]));
+// console.log(JSON.parse(JSON.stringify(Obj, ["marks"])));
+// console.log(Obj);
+
+/* Correct Output
+
+ */
+
 /* Question 3 - What is the output of the following code? */
 
-const a = {};
-const b = { key: "b" };
-const c = { key: "c" };
+// const a = {};
+// const b = { key: "b" };
+// const c = { key: "c" };
 
-a[b] = 123; //  is basically a["[object Object]"]
-a[c] = 456; // so is a["[object Object]"], this the second value of 456(latest) is taken
+// a[b] = 123; //  is basically a["[object Object]"]
+// a[c] = 456; // so is a["[object Object]"], this the second value of 456(latest) is taken
 
-console.log(a);
+// console.log(a); // { '[object Object]': 456 }
 
-console.log(a[b]);
+// console.log(a[b]); //456
 /* Correct Output
 
 {"[object Object]": 456} //when JS tries to convert the key (which is an object)
@@ -69,26 +96,6 @@ Object
 /* Correct Output
 { a: "three",b: "two"}
  */
-
-//// const Obj = { name: "abc", marks: 99, address: "bcd_street" };
-// console.log(JSON.stringify(Obj));
-// console.log(JSON.stringify(Obj, ["marks"]));
-// console.log(JSON.parse(JSON.stringify(Obj, ["marks"])));
-// console.log(Obj);
-// const a = {};
-// const b = {key:'b'};
-// const c = {key :'c'};
-
-// a[b] = 123;
-// console.log(a[b]); //123
-
-// a[c] = 456;
-// console.log(a[c]); //456
-
-// console.log(a) // { '[object Object]': 456 }
-
-// console.log(a[b]) //456
-// console.log(a[c]) //456
 
 /* Question */
 
