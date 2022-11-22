5a632b5b425230b44b72a853a99e61a6d34539fa
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-22 05:46:59 +0530

    intersting output qn

--- 6-Objects/main.js
+++ 6-Objects/main.js
@@ -4,27 +4,47 @@ a name(or key) and a value. A property's value can be a function, in which case 
 property is known as a method.
 */
 
+/* Question 3 - What is the output of the following code? */
+
+const a = {};
+const b = { key: "b" };
+const c = { key: "c" };
+
+a[b] = 123; //  is basically a["[object Object]"]
+a[c] = 456; // so is a["[object Object]"], this the second value of 456(latest) is taken
+
+console.log(a);
+
+console.log(a[b]);
+/* Correct Output
+
+{"[object Object]": 456} //when JS tries to convert the key (which is an object)
+
+456
+
+ */
+
 /* Question 2 - Create a function multiplyByTwo(obj) that multiples all numeric
 property values of nums by 2.*/
 
-let nums = {
-  a: 100,
-  b: 200,
-  title: "My nums",
-};
+// let nums = {
+//   a: 100,
+//   b: 200,
+//   title: "My nums",
+// };
 
-function multiplyByTwo(nums) {
-  for (key in nums) {
-    if (typeof nums[key] === "number") {
-      nums[key] *= 2;
-    }
-  }
+// function multiplyByTwo(nums) {
+//   for (key in nums) {
+//     if (typeof nums[key] === "number") {
+//       nums[key] *= 2;
+//     }
+//   }
 
-  return nums;
-}
+//   return nums;
+// }
 
-multiplyByTwo(nums);
-console.log(nums);
+// multiplyByTwo(nums);
+// console.log(nums);
 
 /* Correct Output
 
