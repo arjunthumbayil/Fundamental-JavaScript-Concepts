ff99e2af2d140dcdabe9effcfaee27f9dae11911
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-22 06:16:24 +0530

    ..."spread " operator

--- 6-Objects/main.js
+++ 6-Objects/main.js
@@ -4,26 +4,34 @@ a name(or key) and a value. A property's value can be a function, in which case 
 property is known as a method.
 */
 
+/* Question 5 - What is the output?*/
+
+console.log([..."Lydia"]);
+
+/* Correct Output
+["L", "y", "d", "i", "a"]
+ */
+
 /* Question 4 - What's JSON.stringify and JSON.parse ? */
 
-const obj = { name: "abc", marks: 99, address: "bcd_street" };
-const strObj = JSON.stringify(obj);
-console.log(strObj); // {"name":"abc","marks":99,"address":"bcd_street"}
+// const obj = { name: "abc", marks: 99, address: "bcd_street" };
+// const strObj = JSON.stringify(obj);
+// console.log(strObj); // {"name":"abc","marks":99,"address":"bcd_street"}
 
-console.log(strObj.name); // is undefined as strObj is a string now
+// console.log(strObj.name); // is undefined as strObj is a string now
 
-const objObj = JSON.parse(strObj);
-console.log(objObj); // {"name":"abc","marks":99,"address":"bcd_street"}
+// const objObj = JSON.parse(strObj);
+// console.log(objObj); // {"name":"abc","marks":99,"address":"bcd_street"}
 
-localStorage.setItem("test", strObj);
-console.log(JSON.parse(localStorage.getItem("test"))); /* {
-    "name": "abc",
-    "marks": 99,
-    "address": "bcd_street"
-}
-*/
+// localStorage.setItem("test", strObj);
+// console.log(JSON.parse(localStorage.getItem("test"))); /* {
+//     "name": "abc",
+//     "marks": 99,
+//     "address": "bcd_street"
+// }
+// */
 
-console.log(JSON.stringify(obj, ["marks"]));
+// console.log(JSON.stringify(obj, ["marks"]));
 // console.log(JSON.parse(JSON.stringify(Obj, ["marks"])));
 // console.log(Obj);
 
