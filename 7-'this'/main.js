11a5cacca2b84d091575367667c2d7b9e7d99a7a
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-24 07:19:49 +0530

    in normal fn 'this' points to immediate parent

--- 7-'this'/main.js
+++ 7-'this'/main.js
@@ -1,2 +1,22 @@
 // 'this' keyword in JavaScript (Implicit binding)
-//
+
+/* Question 1 - What is the output? */
+
+const user = {
+  firstName: "Rahul",
+  getName() {
+    const firstName = "Samba!";
+    return this.firstName;
+  },
+};
+console.log(user.getName()); //
+
+/* Correct Output
+Rahul
+ */
+
+/* Question */
+
+/* Correct Output
+
+ */
