317aa08054486ead0f6e1ad5465fabea66e2bca2
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-24 07:29:22 +0530

    this inside function return

--- 7-'this'/main.js
+++ 7-'this'/main.js
@@ -1,15 +1,34 @@
 // 'this' keyword in JavaScript (Implicit binding)
 
+/* Question 2 - What is the result of accessing its ref? Why? */
+
+function makeUser() {
+  return {
+    name: "John",
+    ref: this,
+  };
+}
+
+let user = makeUser();
+
+console.log(user.ref.name);
+
+console.log(user); //{name:"John", ref:Window}
+
+/* Correct Output
+
+ */
+
 /* Question 1 - What is the output? */
 
-const user = {
-  firstName: "Rahul",
-  getName() {
-    const firstName = "Samba!";
-    return this.firstName;
-  },
-};
-console.log(user.getName()); //
+// const user = {
+//   firstName: "Rahul",
+//   getName() {
+//     const firstName = "Samba!";
+//     return this.firstName;
+//   },
+// };
+// console.log(user.getName());
 
 /* Correct Output
 Rahul
