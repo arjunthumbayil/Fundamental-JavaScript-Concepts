f32ddf0c2461d970edff00646d6b0d3c971e52b7
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-24 07:40:49 +0530

    this and setTimeout

--- 7-'this'/main.js
+++ 7-'this'/main.js
@@ -1,22 +1,42 @@
 // 'this' keyword in JavaScript (Implicit binding)
 
-/* Question 2 - What is the result of accessing its ref? Why? */
+/* Question 3 - What logs to console the following code snippet? */
 
-function makeUser() {
-  return {
-    name: "John",
-    ref: this,
-  };
-}
-
-let user = makeUser();
-
-console.log(user.ref.name);
-
-console.log(user); //{name:"John", ref:Window}
+const user = {
+  name: "Rahul Kudiyal!",
+  logMessage() {
+    console.log(this.name);
+  },
+};
+setTimeout(user.logMessage, 1000);
 
 /* Correct Output
+Nothing on console or undefined, While setTimeout() function uses the object.logMessage
+as a callback, still, it invokes object.logMessage as a regular function, rather than
+a method.
 
+And during a regular function invocation this equals the global object which is window
+in the case of the browser environment.
+
+ */
+
+/* Question 2 - What is the result of accessing its ref? Why? */
+
+// function makeUser() {
+//   return {
+//     name: "John",
+//     ref: this,
+//   };
+// }
+
+// let user = makeUser();
+
+// console.log(user.ref.name);
+
+// console.log(user); //{name:"John", ref:Window}
+
+/* Correct Output
+Nothing, as it points to window object.
  */
 
 /* Question 1 - What is the output? */
