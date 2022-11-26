ee96bd72b907dbe049ccd25dd4b839c92e0fd43e
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-26 06:42:44 +0530

    Bind example

--- 8-Call_Apply_Bind /main.js
+++ 8-Call_Apply_Bind /main.js
@@ -2,24 +2,42 @@
 
  */
 
-/* Question 2 - What is Apply? */
+/* Question 3 - What is Apply? */
 
-function sayHello(day, status) {
-  return (
-    "Hello " +
-    this.name +
-    "," +
-    " today is " +
-    day +
-    " and do feel " +
-    status +
-    "!"
-  );
+function sayHello(age, profession) {
+  return "Hello " + this.name + " is " + age + " and is " + profession;
 }
 
 var obj = { name: "John" };
 
-console.log(sayHello.apply(obj, ["tuesday", "good"]));
+const helloFn = sayHello.bind(obj); //We can reuse hellFn function in Bind
+
+console.log(helloFn(25, "a Software Engineer."));
+console.log(helloFn(29, "a Technical Lead."));
+
+/* Correct Output
+Hello John is 25 and is a Software Engineer.
+Hello John is 29 and is a Technical Lead.
+ */
+
+/* Question 2 - What is Apply? */
+
+// function sayHello(day, status) {
+//   return (
+//     "Hello " +
+//     this.name +
+//     "," +
+//     " today is " +
+//     day +
+//     " and do feel " +
+//     status +
+//     "!"
+//   );
+// }
+
+// var obj = { name: "John" };
+
+// console.log(sayHello.apply(obj, ["tuesday", "good"]));
 
 /* Correct Output
 Hello John, today is tuesday and do feel good!
