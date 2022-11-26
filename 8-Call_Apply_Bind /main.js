af8d8e0d7f448ff0cc41a48377e370319d097e7e
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-26 06:30:03 +0530

    Apply example

--- 8-Call_Apply_Bind /main.js
+++ 8-Call_Apply_Bind /main.js
@@ -1,15 +1,38 @@
-/* Call Bind & Apply
+/* Call Apply & Bind
 
+ */
+
+/* Question 2 - What is Apply? */
+
+function sayHello(day, status) {
+  return (
+    "Hello " +
+    this.name +
+    "," +
+    " today is " +
+    day +
+    " and do feel " +
+    status +
+    "!"
+  );
+}
+
+var obj = { name: "John" };
+
+console.log(sayHello.apply(obj, ["tuesday", "good"]));
+
+/* Correct Output
+Hello John, today is tuesday and do feel good!
  */
 
 /* Question 1 - What is Call?*/
 
-var obj = { name: "John" };
-function sayHello(age) {
-  return "Hello " + this.name + " " + age + "!";
-}
+// var obj = { name: "John" };
+// function sayHello(age) {
+//   return "Hello " + this.name + " " + age + "!";
+// }
 
-console.log(sayHello.call(obj, 28));
+// console.log(sayHello.call(obj, 28));
 
 /* Correct Output
 Hello John 28!
